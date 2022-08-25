faf2264c06943a1616ae7189ff1eb9dd5f7f7c64
sistemas soporte <sistemas@aassa.local>
2022-08-25 15:01:42 -0500

    Se crea el archivo promedio.html y promedio.js. Adicionalmente se hace la operación pero con For

--- promedio.js
+++ promedio.js
@@ -1,11 +1,11 @@
 const lista1 = [100, 200, 300, 500];
 
 function calcularMediaAritmetica(lista) {
-  let sumaLista = 0;
+  // let sumaLista = 0;
 
-  for (let index = 0; index < lista.length; index++) {
-    sumaLista = sumaLista + lista1[index];
-  }
+  // for (let index = 0; index < lista.length; index++) {
+  //   sumaLista = sumaLista + lista1[index];
+  // }
 
   const promedioLista = sumaLista / lista.length;
 
