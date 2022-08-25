a29e8d2f3aa855bf4e9ae3639e489037af690e85
Felipe Ramirez <feliperamirezyepes@gmail.com>
2022-08-25 16:57:56 -0500

    Se crean archivos mediana.html y mediana.js, se empieza el proceso para sacar los datos de la mitad

---
+++ mediana.js
@@ -0,0 +1,17 @@
+const lista1 = [100, 200, 500, 800, 400000000];
+
+const mitadLista1 = parseInt(lista1.length / 2);
+
+function esPar(numerito) {
+  if (numerito % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let mediana;
+
+if (esPar(lista1.length)) {
+} else {
+  mediana = lista1[mitadLista1];
+}
