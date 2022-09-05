c3490fafc0b9683e63d9a96ea37d09460a22d86f
Felipe Ramirez <feliperamirezyepes@gmail.com>
2022-09-05 09:53:45 -0500

    Se finaliza el metodo para calcular la mediana de manera correcta

--- mediana.js
+++ mediana.js
@@ -1,4 +1,4 @@
-const lista1 = [100, 200, 500, 400000000];
+const lista1 = [100, 300, 800, 200, 500, 400000000];
 
 const mitadLista1 = parseInt(lista1.length / 2);
 
@@ -10,12 +10,13 @@ function esPar(numerito) {
   }
 } // aca validamos si es par o impar el array para ver como se procede con la formula de la mediana
 let mediana;
+function calcularMediana(lista) {
+  if (esPar(lista.length)) {
+    const elemento1 = lista1[mitadLista1 - 1];
+    const elemento2 = lista1[mitadLista1];
 
-if (esPar(lista1.length)) {
-  const elemento1 = lista1[mitadLista1 - 1];
-  const elemento2 = lista1[mitadLista1];
-
-  mediana = (elemento1 + elemento2) / 2;
-} else {
-  mediana = lista1[mitadLista1];
+    mediana = (elemento1 + elemento2) / 2;
+  } else {
+    mediana = lista1[mitadLista1];
+  }
 }
